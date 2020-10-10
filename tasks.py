999e4e7c031dd7ae105500c2bcce8bdca05fc539
arslan5555 <64790145+arslan5555@users.noreply.github.com>
2020-10-10 08:19:23 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -6,19 +6,9 @@ from collections import Counter
 from celery import Celery
 
 app = Celery('tasks', backend='rpc://', broker='amqp://a_ham:asdfgh123@192.168.2.221:5672/myvhost')
-
 @app.task
 def file_function(file_name):
-
-	count_han=0
-	count_hon=0
-	count_den=0
-	count_det=0
-	count_denna=0
-	unique_tweets=0
-	count_denne=0
-	count_hen=0
-	counter=0
+	count_han=0,count_hon=0,count_den=0,count_det=0,count_denna=0,unique_tweets=0,count_denne=0,count_hen=0,counter=0
 	with open('/home/ubuntu/data/'+ file_name) as f:
 		for line in f:
 			if line.strip():
@@ -40,7 +30,5 @@ def file_function(file_name):
 						count_denne +=1
 					if sum(1 for match in re.finditer(r"\bhen\b",string)) >0:
 						count_hen +=1
-
 	D = Counter({'han' : count_han, 'hon' : count_hon, 'den' : count_den, 'det' : count_det, 'denna' : count_denna, 'denne':count_denne, 'hen': count_hen})
 	return D,unique_tweets
-
